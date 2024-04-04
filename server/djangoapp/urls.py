6f851404fec76d11810119d93d860ffd306ec88e
mdsimeon <msimeon@apps.salcc.edu.lc>
2024-04-04 04:20:30 -0400

    Week 3 car make car model update

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
      # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+        path(route='get_cars', view=views.get_cars, name ='getcars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
